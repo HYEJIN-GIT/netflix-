fa2b13d10286dd38863d494c01c58703e8dd443b
HYEJIN KWON <chahaa0189@gmail.com>
2026-04-11 16:38:52 +0900

    banner 기능 구현

--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Banner from './components/Banner'
+import Banner from './components/Banner/Banner'
 //1. 배너 =>popular 영화를 들고와서 첫번째 아이템 보여주기
 //2. popular movie
 //3. top rated movie
@@ -7,7 +7,7 @@ import Banner from './components/Banner'
 const Homepage = () => {
   return (
     <div>
-    홈페이지
+    <Banner></Banner>
     </div>
   )
 }
